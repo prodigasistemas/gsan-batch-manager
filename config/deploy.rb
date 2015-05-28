f9f227a6f46ccc3a22c20fdf9761df5039b2c980
Luiz Sanches <luizgrsanches@gmail.com>
2015-05-28 17:14:05 -0300

    obter o repositorio do projeto via https

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.4.0'
 
 set :application, 'gsan-batch-manager'
-set :repo_url, 'git@github.com:prodigasistemas/gsan-batch-manager.git'
+set :repo_url, 'https://github.com/prodigasistemas/gsan-batch-manager.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
